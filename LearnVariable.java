979d91eae38d2f9517170818691b6222b3b95a79
andreanojeremisaragih <andreanosaragih02@gmail.com>
2025-10-21 15:45:02 +0700

    Add scanner umur dan nama

--- LearnVariable.java
+++ LearnVariable.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
 public class LearnVariable {
     public static void main(String[] args) {
     int umur;   
-    String nama;        
+    String nama;
+    
+    Scanner newScan = new Scanner(Syestem. in);
+    
+    
+    System.out.println("Masukkan umur");
     }
 }
